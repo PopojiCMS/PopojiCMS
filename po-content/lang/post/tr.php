e66f30924c99ce6dc2f1b8d6d6b909554de07423
YohanesGuntur <32557669+YohanesGuntur@users.noreply.github.com>
2017-10-06 10:07:36 +0700

    po-content/lang/post/tr.php
    Penambahan bahasa Turkey

---
+++ po-content/lang/post/tr.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ *
+ * - PopojiCMS Post Language
+ *
+ * - File : tr.php
+ * - Version : 1.0
+ * - Author : Jenuar Dalapang
+ * - Developed by: Yohanes Guntur
+ * - License : MIT License
+ *
+*/
+
+$_['component_name'] = 'Post';
+$_['post_title'] = 'başlık | Bağlantı';
+$_['post_title_2'] = 'başlık';
+$_['post_category'] = 'Kategori';
+$_['post_tag'] = 'Tag';
+$_['post_seotitle'] = 'SEO unvanı';
+$_['post_content'] = 'içerik';
+$_['post_picture'] = 'resim';
+$_['post_picture_description'] = 'resim tanımı';
+$_['post_headline'] = 'başlık ayarla';
+$_['post_not_headline'] = 'başlık ayarlı değil';
+$_['post_active'] = 'aktif';
+$_['post_not_active'] = 'etkin değil';
+$_['post_comment'] = 'yorum';
+$_['post_action'] = 'eylem';
+$_['post_addnew'] = 'Post Ekle';
+$_['post_edit'] = 'güncelleme sonrası';
+$_['post_date'] = 'yayınlama tarihi';
+$_['post_time'] = 'yayın zamanı';
+$_['post_by'] = 'by';
+$_['post_subscribe'] = 'abone ol';
+$_['post_share'] = 'Paylaş';
+$_['post_gallery'] = 'resim galerisi';
+$_['post_message_1'] = 'Post başarıyla eklendi';
+$_['post_message_2'] = 'Post başarıyla güncelleştirildi';
+$_['post_message_3'] = 'Post başarıyla silindi';
+$_['post_message_4'] = 'yeni yazılan veriler eklendi';
+$_['post_message_5'] = 'hata sonrası veri güncelleştirildi';
+$_['post_message_6'] = 'hata silindi Post Data';
+$_['post_picture_2'] = 'resim seçilmemiş';
+$_['post_picture_3'] = 'mevcut görüntü önizleme yok';
+$_['post_picture_4'] = 'görüntü değiştirilmediği takdirde, aşağıdaki herhangi bir seçeneği seçmeyin.';
+$_['post_picture_5'] = 'imaj seçili';
+$_['post_picture_6'] = 'varolan görüntü önizleme';
+$_['post_add_category'] = 'yeni kategori ekle';
+$_['post_refresh_category'] = 'Yenile';
+$_['post_add_tag'] = 'yeni etiket ekle';
+$_['post_oauth_message_1'] = 'Twitter durumu başarıyla güncellendi';
+$_['post_oauth_message_2'] = 'hata güncelleme Twitter durumu';
+$_['post_oauth_message_3'] = 'Facebook durumu başarıyla güncellendi';
+$_['post_oauth_message_4'] = 'hata güncelleme Facebook durumu';
+$_['post_subscribe_message_1'] = 'Post başarıyla aboneye gönderilmiş';
+$_['post_import'] = 'wp ve eski PopojiCMS ithal';
+$_['post_import_from'] = 'Kimden';
+$_['post_back_to_post'] = 'geri gönder';
+$_['post_file'] = 'dosya yükle';
+$_['post_import_help'] = 'Bu komut dosyası Max dosya boyutu kabul eder. 10 MB. Eğer \'t upload lütfen sunucu yapılandırmaları dosyanızın boyutunu kabul etmek için doğrulayın.';
+$_['post_message_7'] = 'Post Data alınan hata';
+$_['post_message_8'] = 'Post başarıyla alındı';
+
+$_['front_post_title'] = 'Post';
+$_['front_post_not_found'] = 'Post bulunamadı';
+$_['front_search_title'] = 'arama';
+$_['front_search_not_found'] = 'arama bulunamadı';
